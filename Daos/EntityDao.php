7ad2ad05942a6365ca747ca7c144fd842aad0d21
unknown <sergiofdez.esp@gmail.com>
2024-10-11 21:03:38 +0200

    Creación de métodos getEntity() y getById() para consultar datos de tablas

--- Daos/EntityDao.php
+++ Daos/EntityDao.php
@@ -6,24 +6,124 @@ class EntityDao
 
     public function __construct()
     {
-        $this->conexion = Conexion::getConexion();  // Usamos la clase que maneja la conexión a la BDD
+        // Usamos la clase que maneja la conexión a la BDD
+        $this->conexion = Conexion::getConexion();
     }
 
     // Método que obtiene el valor de una tabla
-    public function getEntity($tableName, $camposAQuitar)
+    public function getEntity($tabla, $camposAQuitar)
     {
-        $array = [];
-        
+        $entidad = [];
+
         // Hacemos la consulta a la base de datos
-        $sql = "SELECT * FROM usuario ";
+        $sql = "SELECT * FROM `$tabla`";
+        //Hacemos la consulta sql
         $resultado = $this->conexion->query($sql);
 
+        //Si hemos obtenido algo de la consulta
         if ($resultado->num_rows > 0) {
+
+            //Cada registro obtenido
             while ($fila = $resultado->fetch_assoc()) {
-                $array[] = $fila;
+
+                $entidad[] = $fila;
             }
         }
 
-        return $array;
+        return $entidad;
+    }
+
+
+    public function getById($id, $tabla, $camposAQuitar)
+    {
+
+        $entidad = $this->getEntity($tabla, $camposAQuitar);
+
+        $valorRegistro = [];
+
+        foreach ($entidad as $registro) {
+
+            if (isset($registro['id'])  && $registro['id'] == $id) {
+                $valorRegistro = $registro;
+            }
+        }
+
+        return $valorRegistro;
+    }
+
+    //Función para obtener los campos de una entidad
+    public function getFields()
+    {
+        return null;
+    }
+
+    //Método para insertar un registro en una tabla
+    public function insertEntity($tabla, $data)
+    {
+
+        //Lo primero que hacemos es obtener los nombres de las columnas y los valores a isnertar
+        //Ejemplo de lo que nos llega "nombre, email, contraseña"
+        $columnas =  implode(', ', array_keys($data));
+
+        //cadena con el número total de interrogaciones
+        $placeholders = implode(", ", array_fill(0, count($data), "?"));
+
+        //Preparamos la consulta
+        $sql = "INSERT INTO $tabla ($columnas) VALUES ($placeholders)";
+        //Preparamos la sentencia
+        $sentencia = $this->conexion->prepare($sql);
+
+        //Cadenad de los tipos para los bindParam
+        $tipos = implode(', ', $this->tiposArray($data));
+
+
+        //Vinculamos los parámetros
+        $sentencia->bind_param($tipos, ...$data);
+
+        //Ejecutamos la consulta 
+        if ($sentencia->execute()) {
+
+            //Retornamos id del nuevo registro insertado
+            return $this->conexion->insert_id;
+        } else {
+
+            return new Exception("Error al insertar " . $sentencia->error);
+        }
+    }
+
+    //Función que devuelve en un array los tipos de un array 
+    private function tiposArray($data)
+    {
+
+        $tipos = [];
+
+        foreach ($data as $indice => $valor) {
+
+            if (is_int($valor)) {
+                $tipos[] = 'i';
+            } elseif (is_double($valor) || is_float($valor)) {
+                $tipos[] = 'd';
+            } elseif (is_string($valor)) {
+                $tipos[] = 's';
+            } elseif (is_null($valor)) {
+                $tipos[] = 's';
+            } else {
+                throw new Exception('Error, tipo no soportado...' . gettype($valor));
+            }
+        }
+
+        return $tipos;
+    }
+
+    public function deleteById($id, $nombreTabla)
+    {
+
+        $sql = "DELETE FROM $nombreTabla WHERE $id = ?";
+        $sentencia = $this->conexion->prepare($sql);
+        $sentencia->bind_param('i', $id);
+
+        $eliminado = $sentencia->execute();
+
+        return $eliminado;
     }
 }
--- api/perfil.php
+++ api/perfil.php
@@ -6,9 +6,8 @@ include_once '../Daos/Conexion.php'; // Asegúrate de incluir también la clase 
 
 // Crear una instancia de Conexion para establecer la conexión
 
-header("Access-Control-Allow-Origin: *");  // Permitir solicitudes desde cualquier origen
-header("Content-Type: application/json; charset=UTF-8");  // Respuesta en formato JSON
-
+header("Access-Control-Allow-Origin: *");  
+header("Content-Type: application/json; charset=UTF-8");  
 
 //new Conexion();
 
@@ -16,7 +15,7 @@ header("Content-Type: application/json; charset=UTF-8");  // Respuesta en format
 $daoEntity = new EntityDao();
 
 // Obtenemos los datos de la base de datos
-$resultado = $daoEntity->getEntity('ususario', []);
+$resultado = $daoEntity->getById(1,"usuarios", []);
 
 // Devolvemos los datos como JSON
 echo json_encode($resultado);
